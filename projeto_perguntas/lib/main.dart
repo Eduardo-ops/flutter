cefb00a8c9c131ad710f25683f63bcc20e9aa21b
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-07-27 14:01:46 -0300

    Arvore de widget implementado

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 
 /* Porta de entra de uma aplicação */
-main() =>
+void main() =>
     /* Arrow Function que nos retorna um widget instanciado. */
-    runApp(new MaterialApp());
+    runApp(new PerguntasApp());
 
 /* Tornando o "perguntaApp" em um widget */
 class PerguntasApp extends StatelessWidget {
-  /*  Método build */
+  /*  Sobrescrevendo método build */
+  @override
   Widget build(BuildContext context) {
     /* Retorna um widget "MaterialApp", que é o principal Widget da aplicação. */
-    return MaterialApp(home: Text('Perguntas App'));
+    return MaterialApp(
+      /* Atributo "home" que retorna um Widget "Scaffold" */
+      home: Scaffold(
+        /* Atributo "appBar" que retorna um Widget "Appbar" */
+        appBar: (AppBar(
+          /* Atributo "Title" que retorna um widget "Text" */
+          title: Text('Questions'),
+        )),
+      ),
+    );
   }
 }
